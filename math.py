ae36a9c7d833b5029bc925e9b56c88d29ca3c822
shubham_dalal <shubham_dalal@persistent.com>
2021-05-28 15:01:20 +0530

    Sub fn implementation

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y      # going in master
 #Sub Implementation
 def sub(x,y):
-    return x-y
+    return x-y      # Bug123
 #Mul Implementation
 def mul(x,y):
     return x*y
