0a25b2dd59eaf756217212861cc992c60adf429b
shubham_dalal <shubham_dalal@persistent.com>
2021-05-28 14:45:44 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,9 +1,13 @@
+#Add implementation
 def add(x,y):
     return x+y
+#Sub Implementation
 def sub(x,y):
     return x-y
+#Mul implementation
 def mul(x,y):
     return x*y
+#Div implementation
 def div(x,y):
     return x/y
     
